c904584bf8f9e41bfaf50797583282a4a2f95a73
Brandon Carlisle <bcarlis2@uncc.edu>
2020-02-06 14:55:00 -0500

    Added basic character, controls, and prefab loading

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraController : MonoBehaviour
+{
+    /*
+     * Camera Controller: Has camera follow player and flip to show behind player
+     */
+
+    [Header("Defaults to GameObject named 'Player'")]
+    public Transform target;
+    public float offsetX = 0f; //Customizable camera distance from player
+    public float offsetY = 2f; //Customizable camera distance from player
+    public float offsetZ = -4f; //Customizable camera distance from player
+    private Vector3 offset;
+    private bool isMirror = false;
+    private Vector3 mirrorOffset;
+    private Quaternion normalRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        normalRot = transform.rotation; //Using starting camera rotation to return to normal after mirror
+        offset = new Vector3(offsetX, offsetY, offsetZ); //Will be used to offset camera from character
+        mirrorOffset = new Vector3(0f,0f,offsetZ + (offsetZ * -3)); //Offset on top of previous offet, moves the camera to the opposite Z position from the player
+        
+        if (target == null)
+        {
+            target = GameObject.Find("Player").transform; //Default target
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset; //Offsets camera from the player
+        transform.rotation = normalRot; //Returns camera rotation to normal after mirror
+
+        if (isMirror)
+        {
+            transform.position += mirrorOffset; //Adds mirror offset on top of normal offset
+            
+            transform.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up); //All I know is this makes the camera rotate 180 degrees to face the player from the front
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.M)) //Mirror
+        {
+            isMirror = true;
+        } else
+        {
+            isMirror = false;
+        }
+
+        if (Input.GetKey(KeyCode.R)) //Resets current scenee
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    /*
+     * Room Manager: Bookkeeping for every room spawned into the game
+     */
+
+    public int maxRooms; //How many rooms will be rendered at any given time
+    public List<Object> roomList = new List<Object>(); //List of all rooms that exist
+    private Object oldRoom; //Room that should be de-rendered
+
+    public void addRoom(Object newRoom)
+    {
+        /*
+         * This part adds all rooms to a basic List, and destroys the old rooms once the list gets too long.
+         * It ideally should keep it in memory, but for now it doesn't. 
+         * A new method of tracking how many rooms are in memory should be implemented eventually.
+         */
+
+        roomList.Add(newRoom); //Adds room to the end of the list
+        Debug.Log("ROOM #" + roomList.IndexOf(newRoom));
+        if (roomList.IndexOf(newRoom) > maxRooms - 1) //If there are now too many rooms loaded
+        {
+            oldRoom = roomList[0]; //Oldest room in the list is at position 0
+            roomList.Remove(roomList[0]); //All rooms automatically shift over
+            Object.Destroy(oldRoom); //Clear room from memory
+        }
+    }
+}
